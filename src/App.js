3efe45e73ed9fe5fdfeec8ccad844c200a6d0abb
Akhil D Nayak <37106902+akhilnayak0206@users.noreply.github.com>
2019-06-10 15:14:10 +0530

    background image added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
-import { Button } from 'react-bootstrap';
+import { Layout, Drawer, Navigation, Content, Header } from 'react-mdl';
 
 function App() {
   return (
-    <div>
-      <Button bsStyle="primary">Primary</Button>
-    </div>
+    <div className="app">
+    <Layout className="bgImage" fixedDrawer>
+    <Header className="header" />
+        <Drawer className="drawer" title="Portfolio">
+            <Navigation className="navLink">
+                <a href="#">Home</a>
+                <a href="#">Projects</a>
+                <a href="#">About Me</a>
+                <a href="#">Contact Me</a>
+            </Navigation>
+        </Drawer>
+        <Content>
+          <h1>Hello</h1>
+        </Content>
+    </Layout>
+</div>
   );
 }
 
