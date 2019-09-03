0a2af71fa324b6d34c95e6f214cab7b2b7b92681
Akhil D Nayak <37106902+akhilnayak0206@users.noreply.github.com>
2019-09-03 14:47:24 +0530

    added redirect to router

--- src/components/MainRouter.js
+++ src/components/MainRouter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 import HomePage from "./HomePage";
 import AboutMe from "./AboutMe";
@@ -9,9 +9,10 @@ import Projects from "./Projects";
 const MainRouter = () => (
   <Switch>
     <Route exact path="/portfolio/" component={HomePage} />
-    <Route exact path="/portfolio/aboutMe" component={AboutMe} />
-    <Route exact path="/portfolio/projects" component={Projects} />
-    <Route exact path="/portfolio/contactMe" component={ContactMe} />
+    <Route path="/portfolio/aboutMe" component={AboutMe} />
+    <Route path="/portfolio/projects" component={Projects} />
+    <Route path="/portfolio/contactMe" component={ContactMe} />
+    <Redirect to='/portfolio' />
   </Switch>
 );
 
