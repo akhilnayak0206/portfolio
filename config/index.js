d1685098fc400db841539bbb413af2c3b711690e
Akhil Nayak <akhil.nayak@codezen.in>
2022-05-05 20:53:54 +0530

    api changes

---
+++ config/index.js
@@ -0,0 +1,5 @@
+export const API_URL = process.env.API_URL || 'http://localhost:3000/api';
+export const SECRET_API_URL = process.env.SECRET_API_URL;
+// export const API_URL = process.env.SECRET_API_URL;
+
+export const NEXT_URL = 'http://localhost:3000';
