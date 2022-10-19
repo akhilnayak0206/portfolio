d4db3bb3741e5b410c025cd4958a35f54dff8234
Akhil Nayak <akhil.nayak@codezen.in>
2022-10-19 18:03:51 +0530

    config

--- config/index.js
+++ config/index.js
@@ -1,5 +1,9 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api';
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api';
 export const SECRET_API_URL = process.env.SECRET_API_URL;
 // export const API_URL = process.env.SECRET_API_URL;
+export const SECRET_API_CALLED = process.env.SECRET_API_CALLED;
+export const NODE_ENV = process.env.NODE_ENV;
+export const CHAT_ID = process.env.CHAT_ID;
 
 export const NEXT_URL = 'http://localhost:3000';
